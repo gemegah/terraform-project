6cdf77635b24bc42f186d16bcc2ea7856e0afb47
Emmanuel <gemegahprince9@gmail.com>
2025-05-12 17:55:11 +0000

    Initial clean commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# Terraform-Project-
+Hosting a static website on AWS using Terraform. (Infrastructure as Code Personal Project) 
+=======
+
+"# terraform-project" 
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+    description = "Bucket name for s3"
+    default = "mybucket-90095"
+}
+
+variable "website_index_document" {
+  description = "name for website index document"
+  default = "index.html"
+}
